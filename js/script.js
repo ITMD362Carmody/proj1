bbed81591c62a82c80ecac35291891a162e9070a
tforthomas73 <decidueye73@gmail.com>
2020-02-26 20:00:12 -0600

    fixed everything

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
 function addJavaScript()
 {
-  var first = document.forms["myForm"]["firstname"].value;
-  if (first == "") {
-    alert("All fields must be completed");
-    return false;
-  }
-  var last = document.forms["myForm"]["lastname"].value;
-  if (last == "") {
-    alert("All fields must be completed");
-    return false;
-  }
   var email = document.forms["myForm"]["email"].value;
   if (email == "") {
-   alert("Email must be filled out");
+   alert("Email field must be completed");
    return false;
   }
 }
