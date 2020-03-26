02916707c7c3a9896a23be18d8162317942901ae
tforthomas73 <decidueye73@gmail.com>
2020-03-25 20:31:11 -0500

    fixed issue with alerts not appearing

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-function addJavaScript()
-{
-  $(document).ready(function(){
-  $("#info_form").submit(function(){
+$(document).ready(function(){
+  $("#myForm").submit(function(){
     if (document.forms["myForm"]["email"].value){
       if (document.forms["myForm"]["firstname"].value){
         if (document.forms["myForm"]["lastname"].value){
           if (document.forms["myForm"]["phone"].value){
-              alert("Your info has been recorded");
+              alert("H4H4 Y0U'V3 B33N H4CK3D F00L");
             } else {
             alert("Phone Number Field Must Be Completed");
             return false;
@@ -25,4 +23,3 @@ function addJavaScript()
     }
   });
 });
-}
